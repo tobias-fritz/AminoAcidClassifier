ef99f38501b615e2b3558fb6991079f176725f19
Tobias Fritz <fritz.tobias@mail.de>
2024-12-31 14:46:49 +0100

    Add AminoAcidCNN_coord_only model for coordinate-only classification

--- src/model.py
+++ src/model.py
@@ -71,5 +71,69 @@ class AminoAcidCNN(nn.Module):
         x = torch.relu(self.fc2(x))
         x = self.softmax(x)
         return x
+    
+class AminoAcidCNN_coord_only(nn.Module):
+    """
+    Convolutional Neural Network for classifying amino acids.
+    """
 
+    def __init__(self, input_channels: int, input_height: int, input_width: int) -> None:
+        """
+        Initialize the AminoAcidCNN model.
 
+        Args:
+            input_channels (int): Number of input channels.
+            input_height (int): Height of the input images.
+            input_width (int): Width of the input images.
+        """
+        super(AminoAcidCNN_coord_only, self).__init__()
+
+        # Add the first convolutional layer
+        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=16, kernel_size=(2, 2), padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=1)
+        
+        # Add the second convolutional layer
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(2, 2), padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        
+        # Calculate the output size after the conv and pool layers
+        self.output_height = input_height +1  # No pooling layers
+        self.output_width = input_width +1  # No pooling layers
+        
+        # Ensure the output dimensions are valid
+        assert self.output_height > 0 and self.output_width > 0, "Output dimensions are too small after conv and pool layers"
+        
+        # Define the fully connected layers
+        self.fc1 = nn.Linear(32 * self.output_height * self.output_width, 128)
+        self.bn_fc1 = nn.BatchNorm1d(128)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(128, 20)
+        self.softmax = nn.Softmax(dim=1)
+
+        # Initialize the weights with Kaiming initialization
+        init.kaiming_uniform_(self.conv1.weight)
+        init.kaiming_uniform_(self.conv2.weight)
+        init.kaiming_uniform_(self.fc1.weight)
+        init.kaiming_uniform_(self.fc2.weight)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the model.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after passing through the network.
+        """
+        x = self.pool(torch.relu(self.bn1(self.conv1(x))))
+        x = torch.relu(self.bn2(self.conv2(x)))
+        #print(f"Shape after conv layers: {x.shape}")  # Print the shape
+
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.bn_fc1(self.fc1(x)))
+        x = self.dropout(x)
+        x = torch.relu(self.fc2(x))
+        x = self.softmax(x)
+        return x
